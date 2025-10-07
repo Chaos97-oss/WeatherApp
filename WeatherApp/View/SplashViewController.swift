468fb741aad5bbc683be1d1eb0edc6ff9b4d84b7
Paul <udegbuepaul43@gmail.com>
2025-10-07 09:07:21 +0100

    In this phase, it works fine but not acceptable

--- WeatherApp/View/SplashViewController.swift
+++ WeatherApp/View/SplashViewController.swift
@@ -11,14 +11,12 @@ class SplashViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
-
+        
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            if let homeVC = storyboard.instantiateViewController(withIdentifier: "HomeViewController") as? HomeViewController {
-                let nav = UINavigationController(rootViewController: homeVC)
-                nav.modalPresentationStyle = .fullScreen
-                self.present(nav, animated: true)
-            }
+            let homeVC = HomeViewController()
+            let nav = UINavigationController(rootViewController: homeVC)
+            nav.modalPresentationStyle = .fullScreen
+            self.present(nav, animated: true)
         }
     }
 }
